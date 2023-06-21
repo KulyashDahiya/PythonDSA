501bae957aefbe596959b428d43cb14984b6ffc0
KulyashDahiya <kulyashdahiya77@gmail.com>
2023-06-21 21:56:05 +0530

    Basics

---
+++ FreeCodingNinjas/Input.py
@@ -0,0 +1,9 @@
+l = [int(i) for i in input().split()]
+print(l)
+
+#Taking n in first element of input
+l = [int(i) for i in input().split()]
+n = l[0]
+print(l)
+l = l[1:]
+print(l)
---
+++ FreeCodingNinjas/Math.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.pi)
+
+print(math.radians(90))
+print(math.degrees(2))
+
+#Trignometry
+print(math.sin(math.radians(30)))
+
+##Absolute Function
+print(abs(5.6))
+print(math.fabs(5.6))
