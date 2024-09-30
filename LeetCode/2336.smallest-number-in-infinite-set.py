4f781ae52e51519ffd2561f9fd3abf074a1cb18b
Kulyash Dahiya <kulyashdahiya77@gmail.com>
2024-09-30 08:49:27 +0530

    LeetCode

---
+++ LeetCode/2336.smallest-number-in-infinite-set.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=2336 lang=python3
+#
+# [2336] Smallest Number in Infinite Set
+#
+
+# @lc code=start
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        
+
+    def popSmallest(self) -> int:
+        
+
+    def addBack(self, num: int) -> None:
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
+# @lc code=end
+
